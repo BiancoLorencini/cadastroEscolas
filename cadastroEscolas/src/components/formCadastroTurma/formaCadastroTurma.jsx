eb541ef35f3cdc24c4bb5b03de03a5d2224896e1
Bianco <arthnilo@gmail.com>
2024-10-24 01:05:43 -0300

    Feat: cadastroTurma com dropDown

--- cadastroEscolas/src/components/formCadastroTurma/formaCadastroTurma.jsx
+++ cadastroEscolas/src/components/formCadastroTurma/formaCadastroTurma.jsx
@@ -1,27 +1,45 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import style from './formaCadastroTurma.module.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const FormCadastroTurma = ({ onSubmit, onCancel }) => {
   const [nomeEscola, setNomeEscola] = useState('');
   const [nomeTurma, setNomeTurma] = useState('');
+  const [escolas, setEscolas] = useState([]);
 
+  useEffect(() => {
+    async function fetchEscolas() {
+      try {
+        const response = await axios.get('http://localhost:3001/escolas');
+        setEscolas(response.data);
+      } catch (error) {
+        console.error('Erro ao buscar escolas:', error);
+      }
+    }
+
+    fetchEscolas();
+  }, [ escolas]);
   const handleSubmit = (event) => {
     event.preventDefault();
     onSubmit({ nomeEscola, nomeTurma });
+    setNomeEscola('');
+    setNomeTurma('');
   };
 
   return (
     <form className={style.form} onSubmit={handleSubmit}>
       <div className={style.inputContainer}>
         <label htmlFor="nomeEscola">Nome da Escola:</label>
-        <input 
-          type="text" 
-          id="nomeEscola" 
-          value={nomeEscola} 
-          onChange={(e) => setNomeEscola(e.target.value)} 
-          required 
-          placeholder="Digite o nome da escola" 
-        />
+        <select className={style.select} name="nomeEscola" id="" value={nomeEscola} onChange={(e) => setNomeEscola(e.target.value)} >
+          <option value="">Selecione...</option>
+          {escolas.map((escola) => (
+            <option key={escola.id} value={escola.nome}>
+              {escola.nome}
+            </option>
+          ))}
+        </select>
       </div>
       <div className={style.inputContainer}>
         <label htmlFor="nomeTurma">Nome da Turma:</label>
